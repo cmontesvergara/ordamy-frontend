00f1268fb6f1d127476af5d062a778fb324a8392
Carlos Montes <krlosbergara@gmail.com>
2026-03-03 19:10:49 -0500

    chore: Update environment URLs for SSO, middleware, base, and callback paths across all environments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    ssoCoreUrl: 'https://back-sso.bigso.co',
+    ssoCoreUrl: 'https://sso-core-prod.bigso.co',
     ssoPortalUrl: 'https://sso.bigso.co',
     appId: 'ordamy',
     baseUrl: 'https://ordamy.bigso.co',
     callbackUrl: '/auth/callback',
-    middlewareBaseUrl: 'https://ordamy-middleware.bigso.co',
+    middlewareBaseUrl: 'https://ordamy-middleware-prod.bigso.co',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
     ssoCoreUrl: 'http://localhost:3000',       // SSO Core
     ssoPortalUrl: 'http://localhost:4201',        // SSO Portal
     appId: 'ordamy',
-    baseUrl: 'http://localhost:4200/',
-    callbackUrl: 'auth/callback',
+    baseUrl: 'http://localhost:4200',
+    callbackUrl: '/auth/callback',
     middlewareBaseUrl: 'http://localhost:4300',    // ordamy-middleware
     mockAuth: true,                         // true = bypass SSO & Permissions locally
 };
