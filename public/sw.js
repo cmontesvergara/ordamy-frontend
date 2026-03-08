31907e307432489935a3dbd92d16322d8fb55a2d
Carlos Montes <krlosbergara@gmail.com>
2026-03-08 17:03:57 -0500

    feat: add basic PWA support by introducing a web manifest and an empty service worker.

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+// Empty service worker to satisfy PWA requirements
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Let the browser do its default thing
+});
