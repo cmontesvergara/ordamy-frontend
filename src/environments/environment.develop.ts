fd4532a51394819ad4d071260e6376a98e489836
Carlos Montes <krlosbergara@gmail.com>
2026-03-22 22:04:27 -0500

    feat: introduce develop build configuration with a dedicated environment file.

---
+++ src/environments/environment.develop.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    ssoCoreUrl: 'https://sso-core-dev.bigso.co',
+    ssoPortalUrl: 'https://sso-dev.bigso.co',
+    appId: 'ordamy',
+    baseUrl: 'https://ordamy-dev.bigso.co',
+    callbackUrl: '/auth/callback',
+    middlewareBaseUrl: 'https://ordamy-middleware-dev.bigso.co',
+};
