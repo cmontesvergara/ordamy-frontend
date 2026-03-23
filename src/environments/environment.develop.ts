03868ad18992fef37160497a1b8d80b9511644e3
Carlos Montes <krlosbergara@gmail.com>
2026-03-23 11:36:59 -0500

    chore: Update development environment appId to a UUID.

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     ssoCoreUrl: 'https://sso-core-dev.bigso.co',
     ssoPortalUrl: 'https://sso-dev.bigso.co',
-    appId: 'ordamy',
+    appId: 'ad7b4e78-a856-486e-9f4c-9fe0a73eb780',//TODO: implementar in edpoint /api/config en el middleware pra que sirva este valor y solo configurrlo en el middleware
     baseUrl: 'https://ordamy-dev.bigso.co',
     callbackUrl: '/auth/callback',
     middlewareBaseUrl: 'https://ordamy-middleware-dev.bigso.co',
