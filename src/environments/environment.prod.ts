136b181c49baa8a3c685c47253155097dea4bf53
Carlos Montes <krlosbergara@gmail.com>
2026-05-04 23:43:37 -0500

    fix: update appId in production environment configuration for correct setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     ssoCoreUrl: 'https://new-sso-back.bigso.co',
     ssoPortalUrl: 'https://new-sso.bigso.co',
-    appId: 'ordamy',
+    appId: '077dcfcb-06f9-4997-9f82-c9bd8dce4c05',//preprod
     baseUrl: 'https://new-ordamy.bigso.co',
     callbackUrl: '/auth/callback',
     middlewareBaseUrl: 'https://new-ordamy-back.bigso.co',
