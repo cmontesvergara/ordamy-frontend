7614586af6e4a02da70c1be0f1a3b8dc2e738ba8
Carlos Montes <krlosbergara@gmail.com>
2026-05-08 00:12:10 -0500

    fix: update environment URLs to point to production endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    ssoCoreUrl: 'https://new-sso-back.bigso.co',
-    ssoPortalUrl: 'https://new-sso.bigso.co',
+    ssoCoreUrl: 'https://sso-core-prod.bigso.co',
+    ssoPortalUrl: 'https://sso.bigso.co',
     appId: '077dcfcb-06f9-4997-9f82-c9bd8dce4c05',//preprod
-    baseUrl: 'https://new-ordamy.bigso.co',
+    baseUrl: 'https://ordamy.bigso.co',
     callbackUrl: '/auth/callback',
-    middlewareBaseUrl: 'https://new-ordamy-back.bigso.co',
-    jwksUrl: 'https://new-sso-back.bigso.co/.well-known/jwks.json',
+    middlewareBaseUrl: 'https://ordamy-middleware-prod.bigso.co',
+    jwksUrl: 'https://sso-core-prod.bigso.co/.well-known/jwks.json',
 };
