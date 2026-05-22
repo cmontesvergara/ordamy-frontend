b96cafec1c056b68150a680030c4296a2d38af56
Carlos Montes <krlosbergara@gmail.com>
2026-05-22 17:12:32 -0500

    ipdate var

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     ssoCoreUrl: 'https://sso-core-prod.bigso.co',
     ssoPortalUrl: 'https://sso.bigso.co',
-    appId: '077dcfcb-06f9-4997-9f82-c9bd8dce4c05',//preprod
+    appId: 'aabf22c8-ca18-4b5f-9405-400cbd9cfcb1',// app ordamy dev en la db prod
     baseUrl: 'https://www.ordamy.com',
     callbackUrl: '/auth/callback',
     middlewareBaseUrl: 'https://ordamy-middleware-dev.bigso.co',
