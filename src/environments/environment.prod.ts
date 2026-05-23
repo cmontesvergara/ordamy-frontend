6c0faf2cf9133813518899a2b4c124fadc25c4dc
Carlos Montes <krlosbergara@gmail.com>
2026-05-22 20:34:49 -0500

    feat: update nginx configuration for SSL support and improve redirect handling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
     ssoCoreUrl: 'https://sso-core-prod.bigso.co',
     ssoPortalUrl: 'https://sso.bigso.co',
     appId: '077dcfcb-06f9-4997-9f82-c9bd8dce4c05',//preprod
-    baseUrl: 'https://ordamy.bigso.co',
+    baseUrl: 'https://www.ordamy.com',
     callbackUrl: '/auth/callback',
-    middlewareBaseUrl: 'https://ordamy-middleware-prod.bigso.co',
+    middlewareBaseUrl: 'https://middleware-prod.ordamy.com',
     jwksUrl: 'https://sso-core-prod.bigso.co/.well-known/jwks.json',
 };
