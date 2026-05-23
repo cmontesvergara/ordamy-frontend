773577f59d7f4a5e52442d8e8d93c7bccdc340cd
Carlos Montes <krlosbergara@gmail.com>
2026-05-22 23:40:57 -0500

    fix: update SSO URLs in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: true,
-    ssoCoreUrl: 'https://sso-core-prod.bigso.co',
-    ssoPortalUrl: 'https://sso.bigso.co',
-    appId: '077dcfcb-06f9-4997-9f82-c9bd8dce4c05',//preprod
-    baseUrl: 'https://www.ordamy.com',
-    callbackUrl: '/auth/callback',
-    middlewareBaseUrl: 'https://middleware-prod.ordamy.com',
-    jwksUrl: 'https://sso-core-prod.bigso.co/.well-known/jwks.json',
+  production: true,
+  ssoCoreUrl: 'https://idp.bigso.org',
+  ssoPortalUrl: 'https://auth.bigso.org',
+  appId: '077dcfcb-06f9-4997-9f82-c9bd8dce4c05', //preprod
+  baseUrl: 'https://www.ordamy.com',
+  callbackUrl: '/auth/callback',
+  middlewareBaseUrl: 'https://middleware-prod.ordamy.com',
+  jwksUrl: 'https://idp.bigso.org/.well-known/jwks.json',
 };
